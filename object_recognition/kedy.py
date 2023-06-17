0a6bbaea2cf7102079380864ef4175f978a88481
Davut Kulaksız <davut.kulaksiz.n7@gmail.com>
2023-06-17 20:17:06 +0300

    feat(object-recognition): train the model with 50 epochs

--- object_recognition/kedy.py
+++ object_recognition/kedy.py
@@ -6,14 +6,14 @@ import cv2
 
 video_path = os.path.join('.', 'forest5.avi')
 
-video_path_out = '{}_out.mp4'.format(video_path)
+video_path_out = '{}_out (50 epochs).mp4'.format(video_path)
 
 cap = cv2.VideoCapture(video_path)
 ret, frame = cap.read()
 H, W, _ = frame.shape
 out = cv2.VideoWriter(video_path_out, cv2.VideoWriter_fourcc(*'MP4V'), int(cap.get(cv2.CAP_PROP_FPS)), (W, H))
 
-model_path = os.path.join('.', 'kedy.pt')
+model_path = os.path.join('.', 'kedy-50.pt')
 
 # Load a model
 model = YOLO(model_path)
